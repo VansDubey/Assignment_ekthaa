94fbdb10695818128c69e1cc7d049f2a0850c688
VansDubey <dvanshika32@gmail.com>
2025-12-11 09:21:51 +0530

    Project done

--- routes/products.py
+++ routes/products.py
@@ -3,13 +3,3 @@ from config.database import db
 
 products_bp = Blueprint("products", __name__)
 
-# GET /catalogue
-@products_bp.route("/catalogue", methods=["GET"])
-def get_catalogue():
-    shop_id = request.args.get("shop_id")
-    products = list(db.products.find({
-        "shop_id": shop_id,
-        "is_visible_in_catalogue": True
-    }, {"_id": 0}))
-
-    return jsonify(products)
